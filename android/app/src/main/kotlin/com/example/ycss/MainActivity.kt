71b9c27dcd163e489bde56b985e9cabccaa0f33c
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-04-17 20:10:05 +0800

    starter code

---
+++ android/app/src/main/kotlin/com/example/ycss/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ycss
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: WelcomePage(),
+  ));
+}
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
