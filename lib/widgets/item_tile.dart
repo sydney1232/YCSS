dbdd12c84c5eb67f11957ab363b44e78c63f9b54
Sydney D. Abuyabor <sydneyabuyabor@gmail.com>
2024-04-27 10:17:45 +0800

    Added onPress Callback for Tile Menu

--- lib/widgets/item_tile.dart
+++ lib/widgets/item_tile.dart
@@ -6,11 +6,13 @@ class ItemTile extends StatefulWidget {
   String name;
   Color backgroundcolor;
   IconData icon;
+  final VoidCallback onPress;
   ItemTile(
       {super.key,
       required this.name,
       required this.backgroundcolor,
-      required this.icon});
+      required this.icon,
+      required this.onPress});
 
   @override
   State<ItemTile> createState() => _ItemTileState();
@@ -19,39 +21,42 @@ class ItemTile extends StatefulWidget {
 class _ItemTileState extends State<ItemTile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-              offset: const Offset(0, 5),
-              color: Theme.of(context).primaryColor.withOpacity(.2),
-              spreadRadius: 2,
-              blurRadius: 5)
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: widget.backgroundcolor,
-              shape: BoxShape.circle,
+    return GestureDetector(
+      onTap: widget.onPress,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+                offset: const Offset(0, 5),
+                color: Theme.of(context).primaryColor.withOpacity(.2),
+                spreadRadius: 2,
+                blurRadius: 5)
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: widget.backgroundcolor,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                widget.icon,
+                color: Colors.white,
+                size: 45,
+              ),
             ),
-            child: Icon(
-              widget.icon,
-              color: Colors.white,
-              size: 45,
-            ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            widget.name,
-            style: Theme.of(context).textTheme.titleMedium,
-          )
-        ],
+            const SizedBox(height: 8),
+            Text(
+              widget.name,
+              style: Theme.of(context).textTheme.titleMedium,
+            )
+          ],
+        ),
       ),
     );
   }
