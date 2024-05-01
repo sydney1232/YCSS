560da7fca6c4f822fc3dbfc912d2931ac92772db
Sydney243 <110798830+Sydney243@users.noreply.github.com>
2024-05-01 13:14:25 +0800

    Changed ADMIN constant email

--- lib/constants/string_constants.dart
+++ lib/constants/string_constants.dart
@@ -4,4 +4,6 @@ const String SIGN_UP = "Sign up";
 const String DONT_HAVE_ACCOUNT = "Don't have an account?";
 const String REGISTER = "Register";
 
-const String ADMIN = "grace@gmail.com";
+const String SELECT_TEAM_UPDATE_SCORE = "Select Team to Score Update";
+
+const String ADMIN = "gmadmin@gmail.com";
