7f1984b7bd4e12b6251a510d8650d78197224cc9
Sydney243 <110798830+Sydney243@users.noreply.github.com>
2024-05-22 21:31:27 +0800

    Align Color Palette

--- lib/constants/color_palette.dart
+++ lib/constants/color_palette.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
-Color blue = Colors.blue.shade900;
-Color white = Colors.white;
-Color lightPink = Colors.pink;
-Color yellowOrange = Colors.orangeAccent.shade200;
+//Run Color Palette
+const blue = Color(0xFF1A4CD4);
+const lightPink = Color(0xFFFF3073);
+const yellowOrange = Color(0xFFFECD00);
+const purple = Color(0xFF9C50E8);
+
+//Side colors
 Color yellowOrange2 = Colors.orangeAccent.shade100;
+const Color white = Colors.white;
