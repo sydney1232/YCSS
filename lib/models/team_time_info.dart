bc5866cf856168d0ab266595d9f7fe194e5e1fc8
Sydney243 <110798830+Sydney243@users.noreply.github.com>
2024-06-02 19:51:49 +0800

    Added class for team time info

---
+++ lib/models/team_time_info.dart
@@ -0,0 +1,13 @@
+class TeamTimeInfo {
+  String docID;
+  String teamName;
+  String time;
+  int timeInMillis;
+
+  TeamTimeInfo({
+    required this.docID,
+    required this.teamName,
+    required this.time,
+    required this.timeInMillis,
+  });
+}
