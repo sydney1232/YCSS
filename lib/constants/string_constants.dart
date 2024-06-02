584592ee2958cf70cf5fed28a9a5174723d9b535
Sydney243 <110798830+Sydney243@users.noreply.github.com>
2024-06-02 19:49:55 +0800

    Added new constants

--- lib/constants/string_constants.dart
+++ lib/constants/string_constants.dart
@@ -10,9 +10,14 @@ const String SELECT_TEAM_UPDATE_SCORE = "Select Team to Score Update";
 const String CAPTURE_THE_FLAG = "Capture the flag";
 const String HEAD_TO_HEAD = "Team Head to Head";
 const String DOUBLE_TAP_TO_DECLARE = "Double Tap to declare the Team Winner";
+const String TEAM_TIME_ATTACK = "Team Time Attack";
+const String TAP_TEAM_GET_TIME = "Tap A team to register time";
 
 const List<String> adminList = [
   "gracecadimas@gmail.com",
   "petercadimas@gmail.com",
   "hanakhaleel@gmail.com"
 ];
+
+const String OK = "OK";
+const String CANCEL = "CANCEL";
